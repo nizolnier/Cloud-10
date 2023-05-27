bff6dfd6ba2c2c3a5234e3b38cf8fac4e8bc5c2a
Viggie1 <haxshapova@gmail.com>
2023-05-27 14:07:02 -0400

    Added DeleteContact enpoint. Also deleted all test files from initial repo structure

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -6,7 +6,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	$conn = new mysqli("localhost", "root", "group@10Slay", "COP4331"); 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
