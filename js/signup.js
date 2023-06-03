8ee0cbb4bb578e2db8da7e4b86ce730420e5688f
Joseph <neo@Thomass-MacBook-Air.local>
2023-06-03 14:55:26 -0400

    Change URL to IP

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://cop4331group10.xyz/LAMPAPI';
+const baseURL = 'http://http://146.190.67.167/LAMPAPI';
 
 sendForm = () => {
     let fname = document.getElementById("fname").value
