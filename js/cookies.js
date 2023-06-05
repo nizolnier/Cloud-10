bfd527e3771cce36dffc342cd7e53e2dddce74e4
Nicole Zolnier <nzolnier@gmail.com>
2023-06-04 20:45:15 -0400

    fix cookies

--- js/cookies.js
+++ js/cookies.js
@@ -10,36 +10,38 @@ saveCookie = (user) => {
     let date = new Date()
     date.setTime(date.getTime() + (minutes * 60 * 1000))
 
-    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + " expires=" + date.toGMTString()
+    document.cookie = "firstName=" + firstName + ";expires=" + date.toGMTString()
+    document.cookie = "lastName=" + lastName + ";expires=" + date.toGMTString()
+    document.cookie = "userId=" + userId + ";expires=" + date.toGMTString()
 }
 
 readCookie = () => {
     userId = -1
     let data = document.cookie
-    let splits = data.split(",")
+    let cookiesArray = data.split(";")
 
-    for (var i = 0; i < splits.length; i++) {
-        let thisOne = splits[i].trim()
-        let tokens = thisOne.split("=")
+    for (let i = 0; i < cookiesArray.length; i++) {
+        let thisOne = cookiesArray[i].trim()
+        let item = thisOne.split("=")
 
-        if (tokens[0] == "firstName") {
-            firstName = tokens[1]
+        if (item[0] == "firstName") {
+            firstName = item[1]
         }
 
-        else if (tokens[0] == "lastName") {
-            lastName = tokens[1]
+        else if (item[0] == "lastName") {
+            lastName = item[1]
         }
 
-        else if (tokens[0] == "userId") {
-            userId = parseInt(tokens[1].trim())
+        else if (item[0] == "userId") {
+            userId = parseInt(item[1].trim())
         }
     }
 
-    /* if (userId < 0) {
+    if (userId < 0) {
         window.location.href = "index.html"
     }
- */
-    //else {
+
+    else {
         document.getElementById("userName").innerHTML = "Welcome, " + firstName + " " + lastName + "!"
-    //}
+    }
 }
